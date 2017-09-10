08c8c88a00106730b711e8bb90682832a2d295c1
Mike Crittenden <mikecrittenden@gmail.com>
2017-09-09 21:20:22 -0400

    Move to cards and add more kids

--- app/src/main/java/com/littlebluelabs/rewardchart/MainActivity.java
+++ app/src/main/java/com/littlebluelabs/rewardchart/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+
+// IMPORTANT NOTE: I KNOW THIS FILE IS ABSOLUTE GARBAGE.
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -23,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         if (points2 != null) {
             ((TextView)findViewById(R.id.pointsView2)).setText(points2);
         }
+
+        String points3 = Integer.toString(prefs.getInt("points3", 0));
+        if (points3 != null) {
+            ((TextView)findViewById(R.id.pointsView3)).setText(points3);
+        }
+
+        String points4 = Integer.toString(prefs.getInt("points4", 0));
+        if (points4 != null) {
+            ((TextView)findViewById(R.id.pointsView4)).setText(points4);
+        }
     }
 
     public void addPoint(View view){
@@ -33,9 +46,15 @@ public class MainActivity extends AppCompatActivity {
         if (view.getId() == R.id.button1) {
             pointsView = ((TextView)findViewById(R.id.pointsView1));
             whichPoints = "points1";
-        } else {
+        } else if (view.getId() == R.id.button3) {
             pointsView = ((TextView)findViewById(R.id.pointsView2));
             whichPoints = "points2";
+        }  else if (view.getId() == R.id.button5) {
+            pointsView = ((TextView)findViewById(R.id.pointsView3));
+            whichPoints = "points3";
+        } else {
+            pointsView = ((TextView)findViewById(R.id.pointsView4));
+            whichPoints = "points4";
         }
 
         int points = Integer.parseInt((String)pointsView.getText());
@@ -54,9 +73,15 @@ public class MainActivity extends AppCompatActivity {
         if (view.getId() == R.id.button2) {
             pointsView = ((TextView)findViewById(R.id.pointsView1));
             whichPoints = "points1";
-        } else {
+        } else if (view.getId() == R.id.button4) {
             pointsView = ((TextView)findViewById(R.id.pointsView2));
             whichPoints = "points2";
+        }  else if (view.getId() == R.id.button6) {
+            pointsView = ((TextView)findViewById(R.id.pointsView3));
+            whichPoints = "points3";
+        } else {
+            pointsView = ((TextView)findViewById(R.id.pointsView4));
+            whichPoints = "points4";
         }
 
         int points = Integer.parseInt((String)pointsView.getText());
